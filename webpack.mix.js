122c05e408fdaa15a1b5edf6d9a05c67e25f31f6
greglongo <gregorylongo@gmail.com>
2021-05-10 23:12:09 -0700

    testing analytics

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+// require('laravel-mix-icomoon');
 
 //new comment
 
@@ -13,5 +14,13 @@ mix.js('static/js/app.js', 'js')
 	.autoload({
 	    jquery: ['$', 'jQuery']
 	})
+// 	.icomoon({
+//     inputPath: 'static/icomoon',
+//     publicPath: 'dist',
+//     output: 'fonts',
+//     cssFile: 'static/scss/base/_fonts.scss',
+//     reload: true,
+//     debug: true
+// });
 	// .copyDirectory('static/svg', 'dist/svg');//incl static assets if needed. extends compile time
 	//
