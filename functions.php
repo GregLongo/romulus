6d2625fe4990314a8960ae69cd10557dc016509f
greglongo <gregorylongo@gmail.com>
2021-05-14 18:49:03 -0700

    responsiveness some bugs and 404

--- functions.php
+++ functions.php
@@ -90,30 +90,18 @@ function count_post_visits() {
 add_action( 'wp_head', 'count_post_visits' );
 
 
-function excerpt($limit) {
-  $excerpt = explode(' ', get_the_excerpt(), $limit);
-  if (count($excerpt)>=$limit) {
-    array_pop($excerpt);
-    $excerpt = implode(" ",$excerpt).'...';
-  } else {
-    $excerpt = implode(" ",$excerpt);
-  }
-  $excerpt = preg_replace('`[[^]]*]`','',$excerpt);
-  return $excerpt;
-}
+//limit excerpt
 
-function content($limit) {
-  $content = explode(' ', get_the_content(), $limit);
-  if (count($content)>=$limit) {
-    array_pop($content);
-    $content = implode(" ",$content).'...';
-  } else {
-    $content = implode(" ",$content);
-  }
-  $content = preg_replace('/[.+]/','', $content);
-  $content = apply_filters('the_content', $content);
-  $content = str_replace(']]>', ']]>', $content);
-  return $content;
+function get_excerpt(){
+$excerpt = get_the_content();
+$excerpt = preg_replace(" ([.*?])",'',$excerpt);
+$excerpt = strip_shortcodes($excerpt);
+$excerpt = strip_tags($excerpt);
+$excerpt = substr($excerpt, 0, 30);
+$excerpt = substr($excerpt, 0, strripos($excerpt, " "));
+$excerpt = trim(preg_replace( '/\s+/', ' ', $excerpt));
+$excerpt = $excerpt.'... <a href="'.get_the_permalink().'">more</a>';
+return $excerpt;
 }
 
 // calendly
