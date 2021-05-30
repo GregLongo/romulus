0d5349dac20f1e483d6df0ce20edd1cd477e38c2
greglongo <gregorylongo@gmail.com>
2021-05-30 10:30:50 -0700

    all sorts of fixes

--- static/js/team.js
+++ static/js/team.js
@@ -18,6 +18,7 @@ $(function() {
     $("#" + contentId).show();
 	});
 
+
   $(".team__teammate").click(function () {
     var memberId = this.id;
     // alert("#" + memberId);
@@ -27,5 +28,21 @@ $(function() {
     $(".team__bio-placeholder").hide();
     $(".team__teammate-bio").hide();
     $("#" + memberContentId).show();
+
+
+
+    $(".bio-tab:first-of-type").addClass("selected");
+    $(".bio__page").hide();
+    $(".bio__page:first-of-type").show();
+
+    $(".bio-tab").click(function () {
+      var tabId = this.id;
+      var contentId = this.id + "_content";
+      $(".bio-tab").removeClass("selected");
+      $("#" + tabId).addClass("selected");
+      $(".bio__page").hide();
+      $("#" + contentId).show();
+    });
+
   });
 });
