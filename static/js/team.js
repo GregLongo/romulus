cf69c2dffa83f8a8f3f3d4f024f2f9a32c23ebb4
greglongo <gregorylongo@gmail.com>
2021-06-24 23:31:48 -0700

    changed erythang

--- static/js/team.js
+++ static/js/team.js
@@ -1,22 +1,12 @@
 $(function() {
-  $(".team-tab:first-of-type").addClass("selected");
-  $(".team__page").hide();
-  $(".team__page:first-of-type").show();
+
 
   $(".team__teammate-bio").hide();
+  $(".team__scrim").hide();
 
   $(".team__teammate").each(function(index){
     $(this).attr('id', 'member-' + index);
   });
-
-	$(".team-tab").click(function () {
-		var tabId = this.id;
-		var contentId = this.id + "_content";
-		$(".team-tab").removeClass("selected");
-		$("#" + tabId).addClass("selected");
-    $(".team__page").hide();
-    $("#" + contentId).show();
-	});
 
 
   $(".team__teammate").click(function () {
@@ -24,25 +14,17 @@ $(function() {
     // alert("#" + memberId);
 		var memberContentId = this.id + "_content";
 		$(".team__teammate").removeClass("selected");
+    $(".team__teammate-bio").removeClass("selected-panel");
 		$("#" + memberId).addClass("selected");
-    $(".team__bio-placeholder").hide();
     $(".team__teammate-bio").hide();
-    $("#" + memberContentId).show();
-
-
-
-    $(".bio-tab:first-of-type").addClass("selected");
-    $(".bio__page").hide();
-    $(".bio__page:first-of-type").show();
-
-    $(".bio-tab").click(function () {
-      var tabId = this.id;
-      var contentId = this.id + "_content";
-      $(".bio-tab").removeClass("selected");
-      $("#" + tabId).addClass("selected");
-      $(".bio__page").hide();
-      $("#" + contentId).show();
-    });
-
+    $("#" + memberContentId).show().addClass("selected-panel");
+    $(".team__scrim").show();
   });
+
+  $(".team__scrim").click(function () {
+    $(".team__teammate-bio").hide();
+    $(".team__scrim").hide();
+  });
+
+
 });
