800b32897c47baaeeb1373cf022b2c69d7dcc13d
twoelevenjay <leon@211j.com>
2021-11-15 21:26:43 -0500

    Home page auto beautify wp standards

--- homepage.php
+++ homepage.php
@@ -1,22 +1,22 @@
 <?php
 /**
-* Template Name: Home Page
-* Description: Landing Page for Sentant
-*/
+ * Template Name: Home Page
+ * Description: Landing Page for Sentant
+ */
 
 
-$context = Timber::get_context();
-$post = new TimberPost();
+$context         = Timber::get_context();
+$post            = new TimberPost();
 $context['post'] = $post;
 
-//query recent posts
-$recent_posts_args = array(
-   'posts_per_page' => 4,
-   'order'=> 'DESC'
+// query recent posts
+$recent_posts_args       = array(
+	'posts_per_page' => 4,
+	'order'          => 'DESC',
 );
-$recent_posts_loop = Timber::get_posts($recent_posts_args);
-$context ['recentposts'] =$recent_posts_loop;
+$recent_posts_loop       = Timber::get_posts( $recent_posts_args );
+$context ['recentposts'] = $recent_posts_loop;
 
 
-Timber::render( 'homepage.twig' , $context );
-$timberContext['options'] = get_fields('options');
+Timber::render( 'homepage.twig', $context );
+$timberContext['options'] = get_fields( 'options' );
